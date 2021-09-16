59fed5bb12a7d8b7c4382a104ce99dda6f4f4b1b
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2021-09-16 15:13:15 +0900

    Define Sonatina IR types

---
+++ crates/codegen/src/ir/types.rs
@@ -0,0 +1,22 @@
+//! This module contains Sonatina IR types definitions.
+
+/// Sonatina IR types definition.
+#[derive(Clone, PartialEq, Eq)]
+pub enum Type {
+    I8,
+    I16,
+    I32,
+    I64,
+    I128,
+    I256,
+    Array { elem_ty: Box<Type>, len: usize },
+}
+
+impl Type {
+    pub fn make_array(elem_ty: Type, len: usize) -> Self {
+        Self::Array {
+            elem_ty: elem_ty.into(),
+            len,
+        }
+    }
+}
