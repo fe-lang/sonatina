1837a0ce54a42e5f7ec082676aa7ac10a12e61e0
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2021-09-17 19:25:07 +0900

    foo

--- crates/codegen/src/ir/dfg.rs
+++ crates/codegen/src/ir/dfg.rs
@@ -2,9 +2,30 @@
 
 use std::collections::HashSet;
 
-use id_arena::Id;
+use id_arena::{Arena, Id};
 
 use super::Value;
+
+pub struct DataFlowGraph {
+    blocks: Arena<BlockData>,
+}
+
+impl DataFlowGraph {
+    pub fn new() -> Self {
+        let mut blocks = Arena::new();
+        blocks.alloc(BlockData {
+            params: HashSet::new(),
+        });
+
+        Self { blocks }
+    }
+
+    fn foo(&self) {
+        for (id, block) in &self.blocks {
+            assert_eq!(self.blocks.get(id).unwrap(), block);
+        }
+    }
+}
 
 /// An opaque reference to [`BlockData`]
 pub type Block = Id<BlockData>;
