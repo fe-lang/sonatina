7ee8c134d198cd785a6c588aef440db58a9cf937
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2021-10-01 21:18:48 +0900

    Implement

--- crates/codegen/src/ir/function.rs
+++ crates/codegen/src/ir/function.rs
@@ -1,4 +1,4 @@
-use super::{DataFlowGraph, Layout, Type};
+use super::{Block, BlockData, DataFlowGraph, Insn, InsnData, Layout, Type};
 
 pub struct Function {
     /// Name of the function.
@@ -14,4 +14,31 @@ pub struct Function {
 pub struct Signature {
     pub args: Vec<Type>,
     pub rets: Vec<Type>,
+}
+
+pub struct FunctionCursor<'a> {
+    func: &'a mut Function,
+    loc: CursorLocation,
+}
+
+pub enum CursorLocation {
+    At(Insn),
+    BlockTop(Block),
+    BlockBottom(Block),
+}
+
+impl<'a> FunctionCursor<'a> {
+    pub fn at(&mut self, loc: CursorLocation) {
+        self.loc = loc;
+    }
+
+    pub fn insert_insn(&mut self, data: InsnData) -> Insn {
+        todo!()
+    }
+
+    pub fn insert_block(&mut self, block_data: BlockData) -> Block {
+        todo!()
+    }
+
+    pub fn remove_insn(&mut self, insn: Insn) {
 }
