a09e80ac7a8ca1e38abdca16f6867710d14896c0
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2021-10-01 11:37:27 +0900

    Add tests for layout

--- crates/codegen/src/ir/layout.rs
+++ crates/codegen/src/ir/layout.rs
@@ -59,9 +59,18 @@ impl Layout {
         self.blocks[&block].last_insn
     }
 
+    pub fn prev_insn_of(&self, insn: Insn) -> Option<Insn> {
+        debug_assert!(self.is_insn_inserted(insn));
+        self.insns[&insn].prev
+    }
+
+    pub fn next_insn_of(&self, insn: Insn) -> Option<Insn> {
+        debug_assert!(self.is_insn_inserted(insn));
+        self.insns[&insn].next
+    }
+
     pub fn insn_block(&self, insn: Insn) -> Block {
         debug_assert!(self.is_insn_inserted(insn));
-
         self.insns[&insn].block
     }
 
@@ -106,15 +115,17 @@ impl Layout {
         debug_assert!(!self.is_block_inserted(block));
 
         let mut block_node = BlockNode::default();
-        let mut before_block_node = self.blocks.get_mut(&before).unwrap();
 
-        match before_block_node.prev {
-            Some(prev) => block_node.prev = Some(prev),
+        match self.blocks[&before].prev {
+            Some(prev) => {
+                block_node.prev = Some(prev);
+                self.blocks.get_mut(&prev).unwrap().next = Some(block);
+            }
             None => self.first_block = Some(block),
         }
 
         block_node.next = Some(before);
-        before_block_node.prev = Some(block);
+        self.blocks.get_mut(&before).unwrap().prev = Some(block);
         self.blocks.insert(block, block_node);
     }
 
@@ -123,14 +134,16 @@ impl Layout {
         debug_assert!(!self.is_block_inserted(block));
 
         let mut block_node = BlockNode::default();
-        let mut after_block_node = self.blocks.get_mut(&after).unwrap();
 
-        match after_block_node.next {
-            Some(next) => block_node.next = Some(next),
+        match self.blocks[&after].next {
+            Some(next) => {
+                block_node.next = Some(next);
+                self.blocks.get_mut(&next).unwrap().prev = Some(block);
+            }
             None => self.last_block = Some(block),
         }
         block_node.prev = Some(after);
-        after_block_node.next = Some(block);
+        self.blocks.get_mut(&after).unwrap().next = Some(block);
         self.blocks.insert(block, block_node);
     }
 
@@ -184,16 +197,19 @@ impl Layout {
         debug_assert!(self.is_insn_inserted(before));
         debug_assert!(!self.is_insn_inserted(insn));
 
-        let before_insn_node = self.insns.get_mut(&before).unwrap();
+        let before_insn_node = &self.insns[&before];
         let block = before_insn_node.block;
         let mut insn_node = InsnNode::new(block);
 
         match before_insn_node.prev {
-            Some(prev) => insn_node.prev = Some(prev),
+            Some(prev) => {
+                insn_node.prev = Some(prev);
+                self.insns.get_mut(&prev).unwrap().next = Some(insn);
+            }
             None => self.blocks.get_mut(&block).unwrap().first_insn = Some(insn),
         }
         insn_node.next = Some(before);
-        before_insn_node.prev = Some(insn);
+        self.insns.get_mut(&before).unwrap().prev = Some(insn);
         self.insns.insert(insn, insn_node);
     }
 
@@ -201,16 +217,19 @@ impl Layout {
         debug_assert!(self.is_insn_inserted(after));
         debug_assert!(!self.is_insn_inserted(insn));
 
-        let after_insn_node = self.insns.get_mut(&after).unwrap();
+        let after_insn_node = &self.insns[&after];
         let block = after_insn_node.block;
         let mut insn_node = InsnNode::new(block);
 
         match after_insn_node.next {
-            Some(next) => insn_node.next = Some(next),
+            Some(next) => {
+                insn_node.next = Some(next);
+                self.insns.get_mut(&next).unwrap().prev = Some(insn);
+            }
             None => self.blocks.get_mut(&block).unwrap().last_insn = Some(insn),
         }
         insn_node.prev = Some(after);
-        after_insn_node.next = Some(insn);
+        self.insns.get_mut(&after).unwrap().next = Some(insn);
         self.insns.insert(insn, insn_node);
     }
 
@@ -300,5 +319,202 @@ impl InsnNode {
             prev: None,
             next: None,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use super::super::dfg::DataFlowGraph;
+
+    impl DataFlowGraph {
+        /// Returns dummy instruction.
+        fn make_insn(&mut self) -> Insn {
+            use super::super::insn::{ImmediateOp, InsnData};
+            let insn_data = InsnData::Immediate {
+                code: ImmediateOp::I8(0),
+            };
+            self.store_insn(insn_data)
+        }
+    }
+
+    #[test]
+    fn test_block_insertion() {
+        let mut layout = Layout::new();
+        let mut dfg = DataFlowGraph::new();
+        assert_eq!(layout.first_block, None);
+        assert_eq!(layout.last_block, None);
+
+        // block1.
+        let b1 = dfg.make_block();
+        layout.append_block(b1);
+        assert_eq!(layout.first_block, Some(b1));
+        assert_eq!(layout.last_block, Some(b1));
+        assert_eq!(layout.prev_block_of(b1), None);
+        assert_eq!(layout.next_block_of(b1), None);
+
+        // block1 -> block2.
+        let b2 = dfg.make_block();
+        layout.append_block(b2);
+        assert_eq!(layout.first_block, Some(b1));
+        assert_eq!(layout.last_block, Some(b2));
+        assert_eq!(layout.prev_block_of(b1), None);
+        assert_eq!(layout.next_block_of(b1), Some(b2));
+        assert_eq!(layout.prev_block_of(b2), Some(b1));
+        assert_eq!(layout.next_block_of(b2), None);
+
+        // block1 -> block3 -> block2.
+        let b3 = dfg.make_block();
+        layout.insert_block_after(b3, b1);
+        assert_eq!(layout.first_block, Some(b1));
+        assert_eq!(layout.last_block, Some(b2));
+        assert_eq!(layout.prev_block_of(b2), Some(b3));
+        assert_eq!(layout.next_block_of(b1), Some(b3));
+        assert_eq!(layout.prev_block_of(b3), Some(b1));
+        assert_eq!(layout.next_block_of(b3), Some(b2));
+
+        // block1 -> block3 -> block4 -> block2.
+        let b4 = dfg.make_block();
+        layout.insert_block_before(b4, b2);
+        assert_eq!(layout.first_block, Some(b1));
+        assert_eq!(layout.last_block, Some(b2));
+        assert_eq!(layout.prev_block_of(b2), Some(b4));
+        assert_eq!(layout.next_block_of(b3), Some(b4));
+        assert_eq!(layout.prev_block_of(b4), Some(b3));
+        assert_eq!(layout.next_block_of(b4), Some(b2));
+    }
+
+    #[test]
+    fn test_block_removal() {
+        let mut layout = Layout::new();
+        let mut dfg = DataFlowGraph::new();
+
+        // block1 -> block2 -> block3 -> block4.
+        let b1 = dfg.make_block();
+        let b2 = dfg.make_block();
+        let b3 = dfg.make_block();
+        let b4 = dfg.make_block();
+        layout.append_block(b1);
+        layout.append_block(b2);
+        layout.append_block(b3);
+        layout.append_block(b4);
+
+        // block1 -> block2 -> block4.
+        layout.remove_block(b3);
+        assert_eq!(layout.first_block, Some(b1));
+        assert_eq!(layout.last_block, Some(b4));
+        assert_eq!(layout.prev_block_of(b4), Some(b2));
+        assert_eq!(layout.next_block_of(b2), Some(b4));
+
+        // block1 -> block2.
+        layout.remove_block(b4);
+        assert_eq!(layout.first_block, Some(b1));
+        assert_eq!(layout.last_block, Some(b2));
+        assert_eq!(layout.prev_block_of(b2), Some(b1));
+        assert_eq!(layout.next_block_of(b2), None);
+
+        // block2.
+        layout.remove_block(b1);
+        assert_eq!(layout.first_block, Some(b2));
+        assert_eq!(layout.last_block, Some(b2));
+        assert_eq!(layout.prev_block_of(b2), None);
+        assert_eq!(layout.next_block_of(b2), None);
+
+        // .
+        layout.remove_block(b2);
+        assert_eq!(layout.first_block, None);
+        assert_eq!(layout.last_block, None);
+    }
+
+    #[test]
+    fn test_insn_insertion() {
+        let mut layout = Layout::new();
+        let mut dfg = DataFlowGraph::new();
+        let b1 = dfg.make_block();
+        layout.append_block(b1);
+        assert_eq!(layout.first_insn_of(b1), None);
+        assert_eq!(layout.last_insn_of(b1), None);
+
+        // insn1.
+        let i1 = dfg.make_insn();
+        layout.append_insn(i1, b1);
+        assert_eq!(layout.first_insn_of(b1), Some(i1));
+        assert_eq!(layout.last_insn_of(b1), Some(i1));
+        assert_eq!(layout.insn_block(i1), b1);
+        assert_eq!(layout.prev_insn_of(i1), None);
+        assert_eq!(layout.next_insn_of(i1), None);
+
+        // insn1 -> insn2.
+        let i2 = dfg.make_insn();
+        layout.append_insn(i2, b1);
+        assert_eq!(layout.first_insn_of(b1), Some(i1));
+        assert_eq!(layout.last_insn_of(b1), Some(i2));
+        assert_eq!(layout.prev_insn_of(i2), Some(i1));
+        assert_eq!(layout.next_insn_of(i1), Some(i2));
+
+        // insn1 -> insn3 -> insn2.
+        let i3 = dfg.make_insn();
+        layout.insert_insn_after(i3, i1);
+        assert_eq!(layout.first_insn_of(b1), Some(i1));
+        assert_eq!(layout.last_insn_of(b1), Some(i2));
+        assert_eq!(layout.next_insn_of(i1), Some(i3));
+        assert_eq!(layout.prev_insn_of(i2), Some(i3));
+        assert_eq!(layout.prev_insn_of(i3), Some(i1));
+        assert_eq!(layout.next_insn_of(i3), Some(i2));
+
+        // insn1 -> insn3 -> insn4 -> insn2.
+        let i4 = dfg.make_insn();
+        layout.insert_insn_before(i4, i2);
+        assert_eq!(layout.first_insn_of(b1), Some(i1));
+        assert_eq!(layout.last_insn_of(b1), Some(i2));
+        assert_eq!(layout.next_insn_of(i3), Some(i4));
+        assert_eq!(layout.prev_insn_of(i2), Some(i4));
+        assert_eq!(layout.prev_insn_of(i4), Some(i3));
+        assert_eq!(layout.next_insn_of(i4), Some(i2));
+    }
+
+    #[test]
+    fn test_insn_removal() {
+        let mut layout = Layout::new();
+        let mut dfg = DataFlowGraph::new();
+        let b1 = dfg.make_block();
+        layout.append_block(b1);
+
+        // insn1 -> insn2 -> insn3 -> insn4.
+        let i1 = dfg.make_insn();
+        let i2 = dfg.make_insn();
+        let i3 = dfg.make_insn();
+        let i4 = dfg.make_insn();
+        layout.append_insn(i1, b1);
+        layout.append_insn(i2, b1);
+        layout.append_insn(i3, b1);
+        layout.append_insn(i4, b1);
+
+        // insn1 -> insn2 -> insn4.
+        layout.remove_insn(i3);
+        assert_eq!(layout.first_insn_of(b1), Some(i1));
+        assert_eq!(layout.last_insn_of(b1), Some(i4));
+        assert_eq!(layout.next_insn_of(i2), Some(i4));
+        assert_eq!(layout.prev_insn_of(i4), Some(i2));
+
+        // insn1 -> insn2.
+        layout.remove_insn(i4);
+        assert_eq!(layout.first_insn_of(b1), Some(i1));
+        assert_eq!(layout.last_insn_of(b1), Some(i2));
+        assert_eq!(layout.next_insn_of(i1), Some(i2));
+        assert_eq!(layout.prev_insn_of(i2), Some(i1));
+
+        // insn2.
+        layout.remove_insn(i1);
+        assert_eq!(layout.first_insn_of(b1), Some(i2));
+        assert_eq!(layout.last_insn_of(b1), Some(i2));
+        assert_eq!(layout.next_insn_of(i2), None);
+        assert_eq!(layout.prev_insn_of(i2), None);
+
+        // .
+        layout.remove_insn(i2);
+        assert_eq!(layout.first_insn_of(b1), None);
+        assert_eq!(layout.last_insn_of(b1), None);
     }
 }
