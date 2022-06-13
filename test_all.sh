550ce50cfccbc1353a5489f5e421ff0e0ba447ff
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2022-06-13 23:22:49 +0900

    Configure GHA

--- test_all.sh
+++ test_all.sh
@@ -6,4 +6,6 @@ cargo clippy --workspace --all-features --all-targets -- -D clippy::all
 cargo doc --no-deps
 
 cargo test --workspace --all-targets
+# NOTE: --all-targets option doesn't invoke doctest, see https://github.com/rust-lang/cargo/issues/6669
+cargo test --workspace --all-features --doc
 cargo run -p sonatina-filecheck
