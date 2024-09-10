41bd4b469c33b0c63180042ee2cdddd88f967668
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2024-09-10 18:04:53 +0200

    Let `inst_set` macro generate `InstKind` enum from a given inst set

--- crates/macros/src/inst_set.rs
+++ crates/macros/src/inst_set.rs
@@ -6,8 +6,10 @@ pub fn define_inst_set(
     _attr: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
+    let attr_args = syn::parse_macro_input!(_attr as syn::AttributeArgs);
     let item_struct = syn::parse_macro_input!(item as syn::ItemStruct);
-    match InstSet::new(item_struct).and_then(InstSet::build) {
+
+    match InstSet::new(attr_args, item_struct).and_then(InstSet::build) {
         Ok(impls) => quote! {
             #impls
         }
@@ -21,25 +23,64 @@ struct InstSet {
     vis: syn::Visibility,
     ident: syn::Ident,
     insts: Vec<syn::Path>,
+    inst_kind_name: syn::Ident,
 }
 
 impl InstSet {
-    fn new(s: syn::ItemStruct) -> syn::Result<Self> {
+    fn new(args: Vec<syn::NestedMeta>, s: syn::ItemStruct) -> syn::Result<Self> {
         let ident = s.ident;
         let vis = s.vis;
         let insts = Self::parse_insts(&s.fields)?;
-        Ok(Self { vis, ident, insts })
+        let inst_kind_name = Self::parse_inst_kind_name(&args)?;
+
+        Ok(Self {
+            vis,
+            ident,
+            insts,
+            inst_kind_name,
+        })
     }
 
     fn build(self) -> syn::Result<proc_macro2::TokenStream> {
         let inst_set = self.define_inst_set();
         let has_inst_impls = self.impl_has_inst();
         let inst_set_base_impl = self.impl_inst_set_base();
+
+        let inst_kind = self.define_inst_kind();
+
         Ok(quote! {
             #inst_set
             #has_inst_impls
             #inst_set_base_impl
+            #inst_kind
         })
+    }
+
+    fn parse_inst_kind_name(args: &[syn::NestedMeta]) -> syn::Result<syn::Ident> {
+        let make_err = || {
+            Err(syn::Error::new(
+                proc_macro2::Span::call_site(),
+                "`#[inst_set(InstKind = \"{InstKindName}\")]` is required",
+            ))
+        };
+
+        if args.len() != 1 {
+            return make_err();
+        }
+
+        let syn::NestedMeta::Meta(syn::Meta::NameValue(name_value)) = &args[0] else {
+            return make_err();
+        };
+
+        let inst_kind_name = match (name_value.path.get_ident(), &name_value.lit) {
+            (Some(ident), syn::Lit::Str(s)) if ident == "InstKind" => s.value(),
+            _ => return make_err(),
+        };
+
+        Ok(syn::Ident::new(
+            &inst_kind_name,
+            proc_macro2::Span::call_site(),
+        ))
     }
 
     fn parse_insts(fields: &syn::Fields) -> syn::Result<Vec<syn::Path>> {
@@ -100,6 +141,35 @@ impl InstSet {
             impl crate::InstSetBase for #ident {
                 #(#methods)*
             }
+        }
+    }
+
+    fn define_inst_kind(&self) -> proc_macro2::TokenStream {
+        let lt = syn::Lifetime::new("'i", proc_macro2::Span::call_site());
+
+        let variants = self.insts.iter().map(|p| {
+            let variant_name = p.segments.last().unwrap();
+            quote! { #variant_name(&#lt #p) }
+        });
+
+        let variants_mut = self.insts.iter().map(|p| {
+            let variant_name = p.segments.last().unwrap();
+            quote! { #variant_name(&#lt mut #p) }
+        });
+
+        let inst_kind_name = &self.inst_kind_name;
+        let inst_kind_mut_name = quote::format_ident!("{inst_kind_name}Mut");
+
+        let vis = &self.vis;
+
+        quote! {
+             #vis enum #inst_kind_name<#lt> {
+                 #(#variants),*
+             }
+
+             #vis enum #inst_kind_mut_name<#lt> {
+                 #(#variants_mut),*
+             }
         }
     }
 }
