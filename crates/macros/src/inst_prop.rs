a8eb77ec79cf89e502631d8dea92af8ea75c586a
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2024-09-28 19:38:38 +0200

    Subset by comprehension using trait as property

--- crates/macros/src/inst_prop.rs
+++ crates/macros/src/inst_prop.rs
@@ -25,7 +25,7 @@ struct InstProp {
 }
 
 impl InstProp {
-    fn new(arg: syn::Meta, mut item_trait: syn::ItemTrait) -> syn::Result<Self> {
+    fn new(arg: syn::Meta, item_trait: syn::ItemTrait) -> syn::Result<Self> {
         const ERR_MSG: &str = "`type Members = (ty_1, ty_2, .., ty_n)` is required";
         let mut members_opt = None;
         for it in item_trait.items.iter() {
@@ -65,48 +65,65 @@ impl InstProp {
         })
     }
 
-    fn build(self) -> syn::Result<proc_macro2::TokenStream> {
+    fn build(mut self) -> syn::Result<proc_macro2::TokenStream> {
         let sealed_trait = self.impl_sealed_trait();
-        let prop_trait = self.define_prop_trait();
+        let prop_trait = self.define_prop_trait()?;
         let subset_def = self.define_subset();
-        //let subset_impl = self.define_subset_impl();
+        let subset_impl = self.define_subset_impl();
 
         Ok(quote! {
             #sealed_trait
             #prop_trait
             #subset_def
-            //#subset_impl
+            #subset_impl
         })
     }
 
     fn impl_sealed_trait(&self) -> proc_macro2::TokenStream {
         let mod_name = self.sealed_module_name();
         let sealed_trait_name = self.sealed_trait_name();
-        let impls = self.members.iter().map(|path| {
+        let impl_for_members = self.members.iter().map(|path| {
             quote! {
                 impl #sealed_trait_name for #path {}
             }
         });
 
+        let subset_name = &self.subset_name;
+        let lt = syn::Lifetime::new("'i", proc_macro2::Span::call_site());
+        let impl_for_susbet = quote! {
+            impl<#lt> #sealed_trait_name for #subset_name<#lt> {}
+        };
+
         quote! {
             mod #mod_name {
                 use super::*;
 
+                #[doc(hidden)]
                 pub trait #sealed_trait_name {}
-                #(#impls)*
-
+                #(#impl_for_members)*
+                #impl_for_susbet
             }
         }
     }
 
-    fn define_prop_trait(&self) -> proc_macro2::TokenStream {
-        let mut item_trait = self.item_trait.clone();
-        item_trait.items.retain(|item| {
+    fn define_prop_trait(&mut self) -> syn::Result<proc_macro2::TokenStream> {
+        self.item_trait.items.retain(|item| {
             let syn::TraitItem::Type(ty) = item else {
                 return true;
             };
             ty.ident != "Members"
         });
+        if let Some(item) = self
+            .item_trait
+            .items
+            .iter()
+            .find(|item| !matches!(item, syn::TraitItem::Fn(_)))
+        {
+            return Err(syn::Error::new_spanned(
+                item,
+                "only trait method is allowed",
+            ));
+        }
 
         let mut sealed_trait = syn::Path::from(self.sealed_module_name());
         sealed_trait.segments.push(self.sealed_trait_name().into());
@@ -117,13 +134,14 @@ impl InstProp {
             path: sealed_trait,
         };
 
-        item_trait
+        self.item_trait
             .supertraits
             .push(syn::TypeParamBound::Trait(sealed_trait_bound));
 
-        quote! {
+        let item_trait = &self.item_trait;
+        Ok(quote! {
             #item_trait
-        }
+        })
     }
 
     fn define_subset(&self) -> proc_macro2::TokenStream {
@@ -132,14 +150,94 @@ impl InstProp {
 
         let variants = self.members.iter().map(|p| {
             let variant_name = subset_variant_name_from_path(p);
-            quote! { #variant_name(&#lt mut #p) }
+            quote! { #variant_name(&#lt #p) }
+        });
+
+        let constraints = self.members.iter().map(|p| {
+            let trait_name = &self.item_trait.ident;
+            quote! { #p: #trait_name }
         });
         let subset_name = &self.subset_name;
 
         quote! {
-             #vis enum #subset_name<#lt> {
+            #vis enum #subset_name<#lt>
+            where #(#constraints,)*
+             {
                  #(#variants),*
              }
+        }
+    }
+
+    fn define_subset_impl(&self) -> proc_macro2::TokenStream {
+        let subset_name = &self.subset_name;
+        let arms = self.members.iter().map(|p| {
+            let variant_name = subset_variant_name_from_path(p);
+            quote!(
+                id if id == std::any::TypeId::of::<#p>() => {
+                    <&#p as crate::prelude::InstDowncast>::map(isb, inst, Self::#variant_name)
+                }
+            )
+        });
+
+        let lt = syn::Lifetime::new("'i", proc_macro2::Span::call_site());
+        let inst_downcast_impl = quote! {
+            impl<#lt> crate::prelude::InstDowncast for #subset_name<#lt> {
+                fn downcast(isb: &dyn crate::prelude::InstSetBase, inst: &dyn crate::Inst) -> Option<Self> {
+                    match inst.type_id() {
+                        #(#arms)*
+                        _ => None
+
+                    }
+                }
+           }
+        };
+
+        let method_impls = self.item_trait.items.iter().map(|item| {
+            let syn::TraitItem::Fn(f) = item else {
+                unreachable!();
+            };
+            let sig = &f.sig;
+            let func_name = &sig.ident;
+            let args: Vec<_> = sig
+                .inputs
+                .iter()
+                .filter_map(|input| {
+                    if let syn::FnArg::Typed(pat_ty) = input {
+                        Some(&pat_ty.pat)
+                    } else {
+                        None
+                    }
+                })
+                .collect();
+
+            let arms = self.members.iter().map(|p| {
+                let variant_name = subset_variant_name_from_path(p);
+                quote!(
+                    Self::#variant_name(inner) => inner.#func_name(#(#args,)*)
+                )
+            });
+
+            quote! {
+                $sig {
+                    match self {
+                        #(#arms,)*
+
+
+                    }
+                }
+            }
+        });
+
+        let trait_name = &self.item_trait.ident;
+        let trait_impl = quote! {
+            impl<#lt> #trait_name for #subset_name<#lt> {
+                #(#method_impls)*
+            }
+        };
+
+        quote! {
+            #inst_downcast_impl
+            #trait_impl
         }
     }
 
