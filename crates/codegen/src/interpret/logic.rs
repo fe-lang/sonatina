2803eec2937d1b8b383dd8bc2ee386469e06c710
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2024-09-30 16:51:48 +0200

    Define `Interpret` and `Interpretable` with `inst_prop`

--- crates/codegen/src/interpret/logic.rs
+++ crates/codegen/src/interpret/logic.rs
@@ -1,34 +1,34 @@
-use super::{Env, Interpret};
-
 use sonatina_ir::{inst::logic::*, Immediate};
 
+use super::{Interpret, State};
+
 impl Interpret for Not {
-    fn interpret(&self, env: &mut dyn Env) -> Option<Immediate> {
-        let arg = env.lookup_val(*self.arg())?;
+    fn interpret(&self, state: &mut dyn State) -> Option<Immediate> {
+        let arg = state.lookup_val(*self.arg())?;
         Some(!arg)
     }
 }
 
 impl Interpret for And {
-    fn interpret(&self, env: &mut dyn Env) -> Option<Immediate> {
-        let lhs = env.lookup_val(*self.lhs())?;
-        let rhs = env.lookup_val(*self.rhs())?;
+    fn interpret(&self, state: &mut dyn State) -> Option<Immediate> {
+        let lhs = state.lookup_val(*self.lhs())?;
+        let rhs = state.lookup_val(*self.rhs())?;
         Some(lhs & rhs)
     }
 }
 
 impl Interpret for Or {
-    fn interpret(&self, env: &mut dyn Env) -> Option<Immediate> {
-        let lhs = env.lookup_val(*self.lhs())?;
-        let rhs = env.lookup_val(*self.rhs())?;
+    fn interpret(&self, state: &mut dyn State) -> Option<Immediate> {
+        let lhs = state.lookup_val(*self.lhs())?;
+        let rhs = state.lookup_val(*self.rhs())?;
         Some(lhs | rhs)
     }
 }
 
 impl Interpret for Xor {
-    fn interpret(&self, env: &mut dyn Env) -> Option<Immediate> {
-        let lhs = env.lookup_val(*self.lhs())?;
-        let rhs = env.lookup_val(*self.rhs())?;
+    fn interpret(&self, state: &mut dyn State) -> Option<Immediate> {
+        let lhs = state.lookup_val(*self.lhs())?;
+        let rhs = state.lookup_val(*self.rhs())?;
         Some(lhs ^ rhs)
     }
 }
--- crates/codegen/src/interpret/mod.rs
+++ crates/codegen/src/interpret/mod.rs
@@ -1,14 +1,45 @@
-use sonatina_ir::{Immediate, ValueId};
+use sonatina_ir::{inst, Immediate, ValueId};
+use sonatina_macros::inst_prop;
 
 mod arith;
 mod cast;
 mod cmp;
 mod logic;
 
+#[inst_prop(Subset = "Interpretable")]
 pub trait Interpret {
-    fn interpret(&self, env: &mut dyn Env) -> Option<Immediate>;
+    fn interpret(&self, env: &mut dyn State) -> Option<Immediate>;
+
+    type Members = (
+        inst::arith::Neg,
+        inst::arith::Add,
+        inst::arith::Sub,
+        inst::arith::Mul,
+        inst::arith::Sdiv,
+        inst::arith::Udiv,
+        inst::arith::Shl,
+        inst::arith::Shr,
+        inst::logic::Not,
+        inst::logic::And,
+        inst::logic::Or,
+        inst::logic::Xor,
+        inst::cast::Sext,
+        inst::cast::Zext,
+        inst::cast::Trunc,
+        inst::cmp::Lt,
+        inst::cmp::Gt,
+        inst::cmp::Slt,
+        inst::cmp::Sgt,
+        inst::cmp::Le,
+        inst::cmp::Ge,
+        inst::cmp::Sle,
+        inst::cmp::Sge,
+        inst::cmp::Eq,
+        inst::cmp::Ne,
+        inst::cmp::IsZero,
+    );
 }
 
-pub trait Env {
-    fn lookup_val(&self, value: ValueId) -> Option<Immediate>;
+pub trait State {
+    fn lookup_val(&mut self, value: ValueId) -> Option<Immediate>;
 }
