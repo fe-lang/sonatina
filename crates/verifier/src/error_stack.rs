be5129087673adddd1327d67051db0d8cbc23405
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-10-06 15:43:45 +0200

    Fix lint

--- crates/verifier/src/error_stack.rs
+++ crates/verifier/src/error_stack.rs
@@ -13,7 +13,7 @@ impl ErrorStack {
         self.errors.push(err)
     }
 
-    pub fn into_errs_iter<'a>(self, func: &'a Function) -> impl IntoIterator<Item = Error<'a>> {
+    pub fn into_errs_iter(self, func: &Function) -> impl IntoIterator<Item = Error<'_>> {
         self.errors
             .into_iter()
             .map(|(_, err)| Error::new(err, func))
