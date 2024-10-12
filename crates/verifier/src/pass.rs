33ea2ade8fe07a30289325a1cf6dececf99650f2
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-10-12 19:29:36 +0200

    Shrink verification context scope to verification on function-basis

--- crates/verifier/src/pass.rs
+++ crates/verifier/src/pass.rs
@@ -1,16 +1,8 @@
 //! Verification pass
 
-use cranelift_entity::entity_impl;
-
 use crate::{error::ErrorData, VerificationCtx};
 
-#[derive(Clone, Copy, Default, PartialEq, Eq)]
-pub struct PassRef(pub u32);
-entity_impl!(PassRef, "pass");
-
 pub trait VerificationPass {
-    fn new(pass_ref: PassRef) -> Self;
-
     fn run(&mut self, ctx: VerificationCtx);
 
     fn report_nonfatal(&self, ctx: &mut VerificationCtx, errs: &[ErrorData]) {
