dc5984eb7e16cfa367655c605812b2f8bf3b4c04
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-10-13 16:57:11 +0200

    Add test for NonEmpty block pass

--- crates/verifier/src/passes/block/non_empty.rs
+++ crates/verifier/src/passes/block/non_empty.rs
@@ -31,3 +31,82 @@ impl VerificationPass for NonEmpty {
         VerificationResult::Pass
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::fmt::Write;
+
+    use sonatina_ir::{
+        builder::test_util::test_func_builder,
+        inst::control_flow::{Jump, Return},
+        isa::Isa,
+        Type,
+    };
+
+    use super::*;
+
+    #[test]
+    fn non_empty_block() {
+        let (evm, mut builder) = test_func_builder(&[], Type::Unit);
+        let is = evm.inst_set();
+
+        let b0 = builder.append_block();
+        let _b1 = builder.append_block(); // empty
+        let b2 = builder.append_block();
+        let b3 = builder.append_block();
+        let _b4 = builder.append_block(); // empty
+        let _b5 = builder.append_block(); // empty
+        let b6 = builder.append_block();
+
+        builder.switch_to_block(b0);
+        builder.insert_inst_no_result_with(|| Jump::new(is, b2));
+
+        builder.switch_to_block(b2);
+        builder.insert_inst_no_result_with(|| Jump::new(is, b3));
+
+        builder.switch_to_block(b3);
+        builder.insert_inst_no_result_with(|| Jump::new(is, b6));
+
+        builder.switch_to_block(b6);
+        builder.insert_inst_no_result_with(|| Return::new(is, None));
+
+        builder.seal_all();
+
+        let module = builder.finish().build();
+        let func_ref = module.iter_functions().next().unwrap();
+        let func = &module.funcs[func_ref];
+
+        let mut ctx = VerificationCtx::new(func_ref, func);
+        let res = NonEmpty.run(&mut ctx);
+        assert_eq!(res, VerificationResult::Fail);
+
+        let mut err_msgs = String::new();
+
+        let errs = ctx
+            .error_stack
+            .into_errs_iter(func, func_ref)
+            .into_iter()
+            .collect::<Vec<_>>();
+
+        for e in errs {
+            write!(&mut err_msgs, "{}\n", e).unwrap();
+        }
+
+        assert_eq!(
+            "empty block, block1
+trace_info:
+0: block1
+1: func public %test_func() -> unit
+empty block, block4
+trace_info:
+0: block4
+1: func public %test_func() -> unit
+empty block, block5
+trace_info:
+0: block5
+1: func public %test_func() -> unit
+",
+            err_msgs
+        );
+    }
+}
